5cb9afe222ca507aba67f6f2257c953fbd88937c
阿部直也 <n.abe@gemcook.com>
2021-10-09 15:56:03 +0900

    #6 useCallback撮影完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
-import React from 'react';
-import { AuthProvider } from './context/AuthContext';
-import './App.css';
-import Bank from './pages/bank/Bank';
+import React, { useState, useCallback } from 'react';
+import './App.scss';
+import Button from './components/button/Button';
+import Counter from './components/counter/Counter';
+import SubTitle from './components/subTitle/SubTitle';
+import Title from './components/Title/Title';
 
 function App() {
+  const [countA, setCountA] = useState<number>(0);
+  const [countB, setCountB] = useState<number>(0);
+
+  // useCallbackでメモ化
+  const handleCountUpA = useCallback(() => {
+    setCountA((prevCount) => prevCount + 1);
+  }, []);
+
+  const handleCountUpB = useCallback(() => {
+    setCountB((prevCount) => prevCount + 1);
+  }, []);
+
+  console.log('-------------');
+
   return (
-    <AuthProvider>
-      <div className='App'>
-        <h2>#5 useReducer</h2>
-        <Bank />
+    <div className='App'>
+      <Title titleText={'#6 useCallback'} />
+      <SubTitle subTitleText={'緊急アンケート：あなたはA派？それともB派？'} />
+      <div className='itemList'>
+        <div className='item'>
+          <Counter counterTitle={'A派'} count={countA} />
+          <Button buttonText={'もちろんA派！'} onClick={handleCountUpA} />
+        </div>
+        <div className='item'>
+          <Counter counterTitle={'B派'} count={countB} />
+          <Button buttonText={'もちろんB派！'} onClick={handleCountUpB} />
+        </div>
       </div>
-    </AuthProvider>
+    </div>
   );
 }
 
